ff80cd77e9d14f0b61d347454443fcd8682d10c7
LifeBringer <LifeBringer@users.noreply.github.com>
2022-06-03 02:09:34 -0400

    completed core and partial stretch features

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Fast Food Feud
 
+## User Stories
 A local startup has asked Site interns to help them construct their new product - `Fast Food Feud`! They love the name and even have a tagline they workshopped: `Folks' Favorite Friendly Fuel Finder For Food Facts`. The one thing they don't have is a working application.
 
 Here's what they have to say about their vision for the mini-app:
@@ -24,3 +25,26 @@ Once that's done, run the application like so:
 ```bash
 npm run dev
 ```
+
+## Objectives
+ - How React's component-based design is centered around writing functions that return `JSX`.
+ - The way that React enforces a one-way flow of data from parent to child - known as passing `props`.
+ - Iteration in `JSX` and how it's used to render lists of items.
+ - Strategies used to customize component styles using its `props`.
+ - Default `props` and how they can be overridden.
+ - The `useState` hook in React and how it's used to manage data that changes - simply called `state`.
+ - What event handlers like `onClick` are used for and how they can trigger state changes.
+ Conditionally rendering JSX based on `props` or `state` values.
+## Application Features
+### Core Features
+ - [x] Displays the title, tagline description, data source, and instructions in the `Header` and `Instructions` component
+ - [x] Categories are displayed in a column and restaurants are displayed in a row.
+ - [x] `Chip` component updates based on the restaurant and category selected by the user.
+ - [x] When a menu item is selected, the `NutritionalLabel` component is displayed with the nutritional facts of the item.
+### Stretch Features
+ - [x] Render different instructions messages depending on state
+ - [x] Add additional `onClick` handlers to `Chip`
+ - [ ] Break sections of the `App.jsx` file into their own components (`CategoryColumn.jsx`, `RestaurantsRow.jsx`, `MenuDisplay.jsx`, and `DataSource.jsx`).
+
+
+### Walkthrough
--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import * as React from "react"
 // IMPORT ANY NEEDED COMPONENTS HERE
 import { createDataSet } from "./data/dataset"
 import "./App.css"
+import { Header } from "./components/Header/Header"
+import { Instructions } from "./components/Instructions/Instructions"
+import { Chip } from "./components/Chip/Chip"
+import { NutritionalLabel } from "./components/NutritionalLabel/NutritionalLabel"
+import { CategoryColumn } from "./components/CategoryColumn/CategoryColumn"
+import { RestaurantsRow } from "./components/RestaurantsRow/RestaurantsRow"
+import { MenuDisplay } from "./components/MenuDisplay/MenuDisplay"
+import { DataSource } from "./data/datasource"
 
 // don't move this!
 export const appInfo = {
@@ -21,37 +29,90 @@ export const appInfo = {
 const { data, categories, restaurants } = createDataSet()
 
 export function App() {
+  const [selectedCategory, setSelectedCategory] = React.useState(null)
+  const [selectedRestaurant, setSelectedRestaurant] = React.useState(null)
+
+  const selectCategory = (category) => {
+    setSelectedItem(null)
+    setSelectedCategory(category)
+  }
+
+  const selectRestaurant = (restaurant) => {
+    setSelectedItem(null)
+    setSelectedRestaurant(restaurant)
+  }
+
+  const currentMenuItems = data.filter((item) => {
+    return item.food_category === selectedCategory && item.restaurant === selectedRestaurant
+  })
+
+  const [selectedItem, setSelectedItem] = React.useState(null)
+
   return (
     <main className="App">
       {/* CATEGORIES COLUMN */}
       <div className="CategoriesColumn col">
         <div className="categories options">
           <h2 className="title">Categories</h2>
-          {/* YOUR CODE HERE */}
+          {categories.map(category => {
+            return (
+              <Chip key={category}
+                    label={category}
+                    onClick={() => selectCategory(category)}
+                    isActive={selectedCategory === category}
+              />
+            )
+          })}
         </div>
       </div>
 
       {/* MAIN COLUMN */}
       <div className="container">
         {/* HEADER GOES HERE */}
+        <Header title={appInfo.title}
+                tagline={appInfo.tagline}
+                description={appInfo.description}
+        />
 
         {/* RESTAURANTS ROW */}
         <div className="RestaurantsRow">
           <h2 className="title">Restaurants</h2>
-          <div className="restaurants options">{/* YOUR CODE HERE */}</div>
+          <div className="restaurants options">
+            {restaurants.map(restaurant => {
+              return (
+                <Chip key={restaurant}
+                      label={restaurant}
+                      onClick={() => selectRestaurant(restaurant)}
+                      isActive={selectedRestaurant === restaurant}
+                />
+              )
+            })}
+          </div>
         </div>
 
         {/* INSTRUCTIONS GO HERE */}
+        <Instructions instructions={appInfo.instructions.start} />
 
         {/* MENU DISPLAY */}
+        {/* <MenuDisplay menuItems={currentMenuItems} /> */}
         <div className="MenuDisplay display">
           <div className="MenuItemButtons menu-items">
             <h2 className="title">Menu Items</h2>
-            {/* YOUR CODE HERE */}
+            {currentMenuItems.map((item, i) => {
+              return (
+                <Chip 
+                key={i}
+                label={item.item_name}
+                onClick={() => setSelectedItem(item)}
+                isActive={selectedItem && selectedItem.item_name === item.item_name }
+                />
+              )
+            })}
           </div>
 
           {/* NUTRITION FACTS */}
-          <div className="NutritionFacts nutrition-facts">{/* YOUR CODE HERE */}</div>
+          <div className="NutritionFacts nutrition-facts"> {selectedItem ? <NutritionalLabel item={selectedItem} /> : null}
+          </div>
         </div>
 
         <div className="data-sources">
